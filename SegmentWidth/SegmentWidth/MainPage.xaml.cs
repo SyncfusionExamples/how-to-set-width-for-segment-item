53a502913a3f16c58dff9c714d9609938f60353e
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-05-31 13:07:21 +0530

    Segment sample has been added

---
+++ SegmentWidth/SegmentWidth/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Syncfusion.XForms.Border;
+using Syncfusion.XForms.Buttons;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SegmentWidth
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
